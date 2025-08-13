6f46ac8c6ada1e6b4bc5d53da476a6fa25a91ecb
Andressa Leles <leles.andressa@gmail.com>
2025-08-12 21:03:41 -0300

    scroll suave

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-const accordionList = document.querySelectorAll(".js-accordion dt")
-accordionList[0].classList.add("ativo")
-accordionList[0].nextElementSibling.classList.add("ativo")
-    function activeAccordion(){
-        this.classList.toggle("ativo")
-        this.nextElementSibling.classList.toggle("ativo")
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
     }
-accordionList.forEach((item) =>{
-    item.addEventListener('click', activeAccordion)
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
+
+
+function initScrollSuave(){
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+function scrollToSection(event){
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute("href")
+
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    })
+}
+linksInternos.forEach((item) =>{
+    item.addEventListener('click', scrollToSection)
 })
+}
+
+initScrollSuave()
